1386954af6523e96788cdbe4fac89fda8607f6e1
ram <hector.ara98@outlook.com>
2024-11-18 17:28:33 -0700

    Se comentó código para seccionar codigo que no se implementa

--- controllers/venta_publicaciones.js
+++ controllers/venta_publicaciones.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const connectionObject = {
   host: "localhost",
   user: "root",
-  password: "goldenstate777*",
+  password: "*",
   database: "urhomeCUU_",
 };
 module.exports = {
