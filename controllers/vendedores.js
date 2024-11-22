065ef515207da5cc2a90dab796a241f6ce1aaf68
Aleman0510 <alemancardenasaram@gmail.com>
2024-11-22 11:43:28 -0600

    esta es la buena

--- controllers/vendedores.js
+++ controllers/vendedores.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 const connectionObject = {
   host: "localhost",
   user: "root",
-  password: " ",
+  password: "NGCM2005",
   database: "urhomeCUU_",
 };
 module.exports = {
